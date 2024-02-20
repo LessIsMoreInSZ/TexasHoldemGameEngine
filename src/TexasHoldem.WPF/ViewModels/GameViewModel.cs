dd34bfbc1a5a82e41465113f563de640040c6217
sjj <987585547@qq.com>
2024-02-20 17:01:52 +0800

    add ai picture

--- src/TexasHoldem.WPF/ViewModels/GameViewModel.cs
+++ src/TexasHoldem.WPF/ViewModels/GameViewModel.cs
@@ -27,12 +27,16 @@ namespace TexasHoldem.WPF.ViewModels
 
         public GameViewModel()
         {
-            PlayerList = new(Enumerable.Range(1, App.PlayerNumer).
-        Select(n => new Player
-        {
-            Name = $"AI Player {n}",
-            Angle = n * 360d / App.PlayerNumer
-        }));
+            PlayerList = new()
+            {
+                new() { Name = "三重刘德华", Avatar = "/Assets/ldh.jfif" ,Angle=0},
+                new() { Name = "不愿意收手的阿祖", Avatar = "/Assets/wyz.jfif" ,Angle=60},
+                new() { Name = "赌神黄四郎", Avatar = "/Assets/zrf.jfif",Angle=120 },
+                new() { Name = "Player",Avatar="" ,Angle=180},
+                new() { Name = "Solid本体", Avatar = "/Assets/pyy.jfif" ,Angle=240},
+                new() { Name = "寒战总指挥刘sir", Avatar = "/Assets/gfc.jfif" ,Angle=300},
+
+            };
         }
     }
 }
